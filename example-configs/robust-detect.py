f2be08bb208624c405cd3b6ab932b0332d1078b4
Jing Conan Wang <hbhzwj@gmail.com>
2014-07-27 23:51:00 -0400

    update robust-detect.py example-configs

--- example-configs/robust-detect.py
+++ example-configs/robust-detect.py
@@ -4,8 +4,6 @@ import numpy as np
 #################################
 ##   Parameters For Detector  ###
 #################################
-# ANO_ANA_DATA_FILE = ROOT + '/Share/AnoAna.txt'
-ANO_ANA_DATA_FILE = './Share/AnoAna.txt'
 DETECTOR_DESC = dict(
         method = 'robust',
         # method = 'mfmb',
@@ -17,12 +15,12 @@ DETECTOR_DESC = dict(
         fr_win_size=100, # window size for estimation of flow rate
         false_alarm_rate = 0.001,
         unified_nominal_pdf = False, # used in sensitivities analysis
-        fea_option = {'dist_to_center':2, 'flow_size':2, 'cluster':2},
-        # fea_option = {'dist_to_center':1, 'flow_size':2, 'cluster':1},
+        fea_option = {'flow_size':(2, [0, 10000]), 'duration':(3, [0, 10])},
+        # fea_option = {'flow_size':(2, [0, 10000]), 'duration':(3, [0, 10]),
+        #               'cluster': (3, [0, 2])},
         dump_folder = ROOT + 'res/',
-        ano_ana_data_file = ANO_ANA_DATA_FILE,
         normal_rg = None,
-        ref_data = ROOT + 'Simulator/n0_flow.txt',
+        ref_data = ROOT + 'test-data/n0_flow.txt',
         # normal_rg = None,
         detector_type = 'mfmb',
         max_detect_num = None,
@@ -71,25 +69,3 @@ DETECTOR_DESC = dict(
         # small_win_size = 1000,
         g_quan_N = 3,
         )
-# when using different data_hanlder, you will have different set of avaliable options for fea_option.
-
-FLOW_RATE_ESTIMATE_WINDOW = 100 #only for test
-
-FALSE_ALARM_RATE = 0.001 # false alarm rate
-NominalPDFFile = ROOT + '/Share/nominal.p'
-UNIFIED_NOMINAL_PDF = False
-
-
-#########################################
-### Parameters for Derivative Test  ####
-#########################################
-DERI_TEST_THRESHOLD = 0.1
-# DERI_TEST_THRESHOLD = 1
-
-# ANOMALY_TYPE_DETECT_INDI = 'derivative'
-# MODEL_FREE_DERI_DESCENDING_FIG = ROOT + '/res/mf-descending-deri.eps'
-# MODEL_BASE_DERI_DESCENDING_FIG = ROOT + '/res/mb-descending-deri.eps'
-
-ANOMALY_TYPE_DETECT_INDI = 'component'
-MODEL_FREE_DERI_DESCENDING_FIG = ROOT + '/res/mf-descending-comp.eps'
-MODEL_BASE_DERI_DESCENDING_FIG = ROOT + '/res/mb-descending-comp.eps'
